638822b1168a6995d16373bbce4e7a6e266dacc8
hannahbrett17 <hbrett@tcd.ie>
2026-04-01 09:29:47 +0100

    Add files via upload

---
+++ BMSim_overlapping.R
@@ -0,0 +1,98 @@
+# Simulation II: Overlapping Brownian motion mixture
+# Corresponds to Section 2.4.3 of thesis
+
+# load EM functions
+source("../bm_model/bm_em.R")
+
+# --------SIMULATING BMMM--------- #
+# Simulating Brownian motion mixture model to apply EM alg
+
+# uniform time grid for each BM
+# interval length
+t <- 1
+
+# increment length
+dt <- 0.001
+
+# number of increments
+m <- floor( t / dt )
+
+# number of BMs
+n <- 1000
+
+# states
+G <- 3
+
+# parameters
+pi.dist <- c(1/3, 1/3, 1/3)
+mu <- c(0, 0, 0)
+sig2 <- c(0.25, 0.36, 0.49)
+
+# assigning each curve to a component
+set.seed(1234)
+z <- sample(1:G, size=n, replace=TRUE, prob=pi.dist)
+
+# Matrix of BM increments, sum of row is one BM
+Y <- matrix(0, nrow=n, ncol=m)
+
+# making Y 
+for (i in 1:n) {
+  g <- z[i] 
+  Y[i, ] <- rnorm(m, mean = mu[g] * dt, sd = sqrt(sig2[g] * dt))
+}
+
+# Plotting simulated Brownian paths 
+X <- t(apply(Y[1:250,], 1, cumsum))
+X <- cbind(rep(0, 250), X)  
+colours <- c("#49d9de", "#c71e8f", "orange")
+path_colours <- colours[z]
+
+# plotting
+limits <- abs(max(mu) * t) + 2 * sqrt(max(sig2) * t)
+matplot(seq(0, t, by=dt), t(X), type="l", xlab="t", ylab="Y(t)",
+        main="Simulated Brownian Paths",
+        xlim=c(0, t), ylim=limits * c(-1, 1), 
+        lty=1, col=path_colours)
+legend("topleft", legend=paste("Component", sort(unique(z))),
+       col=colours, lty=1, bty="n")
+
+# Running the EM algorithm 
+em <- EM.BMMM(Y, G, dt, maxstep=1000, tol=1e-6, init_func = initialise.variance)
+
+# Results
+em$par
+
+# parameter estimation errors
+mean_est_error <- Metrics::rmse(mu, em$par$psi$mean)
+var_est_error <- Metrics::rmse(sig2, em$par$psi$sig2)
+print(mean_est_error)
+print(var_est_error)
+
+# confusion matrix of component labels and ARI
+z_est <- max.col(em$Gamma.mat)
+table(true = z, est = z_est)
+ari <- mclust::adjustedRandIndex(z, z_est)
+print(ari)
+
+# recovering number of components with BIC
+G_max <- 10
+BIC <- numeric(G_max)
+
+# looping through different numbers of components and returning one with min BIC
+for (g in 2:G_max){
+  em <- EM.BMMM(Y, g, dt, maxstep=1000, tol=1e-6, init_func = initialise.variance)
+  #BIC
+  loglik <- em$loglik
+  p <- 3*g - 1 #3 from each component, pi sums to 1, last is determined
+  BIC[g] <- p*log(n) -2*loglik
+}
+
+# BIC plot
+best_G <- which.min(BIC[-1]) + 1
+cat("Best number of states according to BIC:", best_G, "\n")
+plot(2:G_max, BIC[-1], type='b',
+     main='BIC vs Number of Components', xlab='G', ylab='BIC')
+abline(v=best_G, col='red', lty=2)
+
+
+
---
+++ GPSim_sep.R
@@ -0,0 +1,79 @@
+# Simulation I: Separated GP mixture
+# Corresponds to Section 3.5.1 of thesis
+
+# load EM functions
+source("../gp_model/GaussianProcessEM.R")
+
+# --------SIMULATING GPMM--------- #
+# Simulating GP mixture model to apply EM alg
+
+# Covariance function
+sqex_kernel <- function(X, sig2, l, tau2)
+{
+  D <- as.matrix(dist(X)^2)
+  K <- sig2 * exp( -D / l^2 ) 
+  diag(K) <- diag(K) + tau2
+  return(K)
+}
+
+# Simulating GP mixture model
+
+# number of curves
+n <- 50
+
+# number of time points
+p <- 100
+
+# inputs
+X <- seq(-1,1,length.out=p)
+X <- as.matrix(X)
+
+# number of components
+G <- 2
+
+# parameters
+pi.dist <- c(1/2, 1/2)
+sig2 <- c(1, 2)
+l <- c(0.1, 0.5)
+tau2 <- c(1e-4, 1e-4)
+mu <- matrix(0, G, p)
+
+# assigning each curve to a component
+set.seed(123)
+z <- sample(1:G, size=n, replace=TRUE, prob=pi.dist)
+
+# making Y
+Y <- matrix(0, n, p)
+for (i in 1:n){
+  g <- z[i]
+  K_g <- sqex_kernel(X, sig2[g], l[g], tau2[g])
+  Y[i, ] <- as.numeric(mvtnorm::rmvnorm(1, mean = mu[g, ], sigma = K_g))
+}
+
+# plotting mixture model
+colours <- c("dodgerblue", "red")
+path_colours <- colours[z[31:40]]
+matplot(X, t(Y[31:40,]), type = "l", lwd=2,lty = 1, xlab = "x", ylab = "Y(x)", col = path_colours, 
+        main = "Simulated GPs")
+legend("topright", legend=paste("Component", sort(unique(z[31:40]))),
+       col=colours, lty=1, lwd=2, bty="n")
+
+#---Running the EM algorithm for m=5 neighbours---#
+m <- 5
+em <- EM.GP.restarts( Y, X, G, m, maxstep=1000, tol=1e-6, restarts=10)
+em$par
+
+# parameter estimation errors
+sig2_error <- Metrics::rmse(sig2, em$par$psi$sig2)
+l_error <- Metrics::rmse(l, em$par$psi$l)
+tau2_error <- Metrics::rmse(tau2, em$par$psi$tau2)
+print(sig2_error)
+print(l_error)
+print(tau2_error)
+
+# confusion matrix of component labels and ARI
+z_est <- max.col(em$Gamma.mat)
+table(true = z, est = z_est)
+ari <- mclust::adjustedRandIndex(z, z_est)
+print(ari)
+
